0026281a529285a2c4b849eecb66aa8083dc5d95
Tao Fang <partake@hotmail.com>
2011-12-26 21:09:13 -0600

    Pages for following/followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,26 @@
 class UsersController < ApplicationController				  
-	before_filter :authenticate, :only => [:index, :edit, :update]
+#	before_filter :authenticate, :only => [:index, :edit, :update]
+	before_filter :authenticate, :except => [:show, :new, :create]
 	before_filter :correct_user, :only => [:edit, :update]
 
   def show
     @user = User.find(params[:id])
     @title = @user.name
     @microposts = @user.microposts.paginate(:page => params[:page])
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'
   end
 
   def new
